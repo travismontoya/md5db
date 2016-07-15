e22abafb36396052a880f062f77cc5c91d7a50f4
Travis Montoya <dev.travismontoya@yahoo.com>
2016-07-15 12:53:05 -0700

    Initial commit. Still need to implement search

---
+++ src/md5db.hs
@@ -0,0 +1,57 @@
+module Main where
+
+import qualified Data.ByteString.Lazy.Char8 as LB8 (pack)
+import qualified Data.ByteString.Char8      as B (putStr, putStrLn)
+import qualified Data.ByteString.Internal
+import Data.Char (ord)
+import Crypto.Hash
+import System.Environment (getArgs)
+import System.IO
+
+
+md5Str                :: [Char] -> Digest MD5
+md5Str s              = hashlazy $ LB8.pack s
+
+showMD5               :: [[Char]] -> Data.ByteString.Internal.ByteString
+showMD5 s             = digestToHexByteString $ md5Str $ last s
+
+----------------------------------------------------------------------------------------------
+-- 
+-- Run the program against a word list
+--
+----------------------------------------------------------------------------------------------
+runMD5db              :: FilePath -> IO ()
+runMD5db f            = do withFile f ReadMode (\wf -> do
+                              putStrLn "Running.. This can take awhile!"
+                              w        <- hGetContents wf
+                              let md5s = zip (lines w) hashedWords
+                                   where hashedWords = map md5Str (lines w)
+                              saveDB md5s
+                              putStrLn "Saving database...")
+
+saveDB                :: [([Char], Digest MD5)] -> IO ()
+saveDB m              = do withFile "md5db" WriteMode (\db -> do
+                              sequence_ [hPutStrLn db (x ++ ":" ++ show y) | (x, y) <- m])
+
+----------------------------------------------------------------------------------------------
+-- 
+-- Usage and Main program execution
+--
+----------------------------------------------------------------------------------------------
+parseArgs             :: [[Char]] -> IO ()        
+parseArgs ("run":xs)  = runMD5db $ last xs
+parseArgs ("find":xs) = printUsage
+parseArgs ("md5":s)   = B.putStrLn $ showMD5 s
+parseArgs _           = printUsage
+
+printUsage            :: IO ()
+printUsage            = putStrLn ( "usage: md5db [OPTION] {ARG}\n" ++
+                                   "OPTIONS:\n" ++
+                                   "  run <file>        run md5db on the given wordfile\n" ++
+                                   "  find <str> <file> find <str> in the md5db <file>\n"  ++
+                                   "  md5 <str>         return the md5 of <str>" )
+
+-- Main execution
+main                  :: IO ()
+main                  = do args <- getArgs
+                           parseArgs args
